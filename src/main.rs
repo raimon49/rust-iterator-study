94d5ebc1b59dceb20071b9573f8f42d965325fd6
raimon <raimon49@hotmail.com>
2021-03-28 20:02:05 +0900

    Add a code about using filter_map()

--- src/main.rs
+++ src/main.rs
@@ -117,4 +117,14 @@ fn main() {
         ["earth", "water", "air", "fire"]
             .iter().map(|ert| println!("{}", ert));
     }
+
+    {
+        use std::str::FromStr;
+
+        let text = "1\nfrond .25 289\n3.1415 estuary\n";
+        for number in text.split_whitespace()
+            .filter_map(|w| f64::from_str(w).ok()) {
+                println!("{:4.2}", number.sqrt());
+            }
+    }
 }
