49f93aedcdda71d9c9694df8541cd6545ec6ff85
raimon <raimon49@hotmail.com>
2021-03-29 21:10:54 +0900

    Add a code about using flat_map()

--- src/main.rs
+++ src/main.rs
@@ -139,4 +139,22 @@ fn main() {
                 println!("{:4.2}", number.sqrt());
             }
     }
+
+    {
+        use std::collections::HashMap;
+
+        let mut major_cities = HashMap::new();
+        major_cities.insert("Japan", vec!["Tokyo", "Kyoto"]);
+        major_cities.insert("The United States", vec!["Portland", "Nashville"]);
+        major_cities.insert("Brazil", vec!["Sao Paulo", "Brasilia"]);
+        major_cities.insert("Kenya", vec!["Nairobi", "Mombasa"]);
+        major_cities.insert("The Netherlands", vec!["Amsterdam", "Utrecht"]);
+
+        let countries = ["Japan", "Brazil", "Kenya"];
+
+        for &city in countries.iter().flat_map(|country| &major_cities[country]) {
+            println!("{}", city);
+        }
+
+    }
 }
