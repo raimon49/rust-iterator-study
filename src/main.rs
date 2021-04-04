dc07c5f49a17bf10f2f2ae69b5a4a4730268a721
raimon <raimon49@hotmail.com>
2021-04-04 19:23:06 +0900

    Add a code about using scan() iterator

--- src/main.rs
+++ src/main.rs
@@ -157,4 +157,18 @@ fn main() {
         }
 
     }
+
+    {
+        let iter = (0..10)
+            .scan(0, |sum, item| {
+                *sum += item;
+                if *sum > 10 {
+                    None
+                } else {
+                    Some(item * item)
+                }
+            });
+
+        assert_eq!(iter.collect::<Vec<i32>>(), vec![0, 1, 4, 9, 16]);
+    }
 }
