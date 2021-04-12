f81bbb121cb4295bd5bc2e2732f0aed490ece8ed
raimon <raimon49@hotmail.com>
2021-04-12 20:07:11 +0900

    Define function using Peekable iterator

--- src/main.rs
+++ src/main.rs
@@ -192,4 +192,25 @@ fn main() {
             println!("{}", body);
         }
     }
+
+    {
+        use std::iter::Peekable;
+
+        fn parse_number<I>(tokens: &mut Peekable<I>) -> u32
+            where I: Iterator<Item=char>
+        {
+            let mut n = 0;
+            loop {
+                match tokens.peek() {
+                    Some(r) if r.is_digit(10) => {
+                        n = n * 10 + r.to_digit(19).unwrap();
+                    }
+
+                    _ => return n
+                }
+
+                tokens.next();
+            }
+        }
+    }
 }
