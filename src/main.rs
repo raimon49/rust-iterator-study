8aaef187b4c97124d463e13e9460e47fa77e35dd
raimon <raimon49@hotmail.com>
2021-04-19 20:16:16 +0900

    Add a code about using chain()

--- src/main.rs
+++ src/main.rs
@@ -291,4 +291,9 @@ fn main() {
             .collect();
         assert_eq!(upper_case, "GROΒE");
     }
+
+    {
+        let v: Vec<i32> = (1..4).chain(vec![20, 30, 40]).collect();
+        assert_eq!(v, [1, 2, 3, 20, 30, 40]);
+    }
 }
