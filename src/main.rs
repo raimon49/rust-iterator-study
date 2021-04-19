329f26048e68a40a50bd979b693599fe8f59b7d0
raimon <raimon49@hotmail.com>
2021-04-19 20:17:30 +0900

    Add a code about using chain() and rev()

--- src/main.rs
+++ src/main.rs
@@ -295,5 +295,8 @@ fn main() {
     {
         let v: Vec<i32> = (1..4).chain(vec![20, 30, 40]).collect();
         assert_eq!(v, [1, 2, 3, 20, 30, 40]);
+
+        let rev_v: Vec<i32> = (1..4).chain(vec![20, 30, 40]).rev().collect();
+        assert_eq!(rev_v, [40, 30, 20, 4, 3, 2, 1]);
     }
 }
