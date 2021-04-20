d4fd2424dd8b7722c35d236eaa9336600dd2535c
raimon <raimon49@hotmail.com>
2021-04-20 22:37:23 +0900

    Fix assertion for call chain() and rev()

--- src/main.rs
+++ src/main.rs
@@ -297,6 +297,6 @@ fn main() {
         assert_eq!(v, [1, 2, 3, 20, 30, 40]);
 
         let rev_v: Vec<i32> = (1..4).chain(vec![20, 30, 40]).rev().collect();
-        assert_eq!(rev_v, [40, 30, 20, 4, 3, 2, 1]);
+        assert_eq!(rev_v, [40, 30, 20, 3, 2, 1]);
     }
 }
