d9697cfab528798740a51f2cff09429c85c13fd7
raimon <raimon49@hotmail.com>
2021-05-04 19:22:14 +0900

    Add a code about using max() and min()

--- src/main.rs
+++ src/main.rs
@@ -390,4 +390,9 @@ fn main() {
         }
         assert_eq!(factorial(20), 2432902008176640000);
     }
+
+    {
+        assert_eq!([-2, 0, 1, 0, -2, -5].iter().max(), Some(&1));
+        assert_eq!([-2, 0, 1, 0, -2, -5].iter().min(), Some(&-5));
+    }
 }
